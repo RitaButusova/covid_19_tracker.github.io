4238b57f3509fd179956afc7df4b46e53890202b
RitaButusova <47254480+RitaButusova@users.noreply.github.com>
2020-09-02 22:07:09 +0300

    Updates

---
+++ precache-manifest.f397667603d9d9b134cb1eb3af5896b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fb0498d7d8f9b9d7b5407a31782745",
+    "url": "/covid_19_tracker.github.io/index.html"
+  },
+  {
+    "revision": "bd493866425e447dd24a",
+    "url": "/covid_19_tracker.github.io/static/css/main.f973c507.chunk.css"
+  },
+  {
+    "revision": "55a1418cc2256082eb5f",
+    "url": "/covid_19_tracker.github.io/static/js/2.82210647.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_19_tracker.github.io/static/js/2.82210647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd493866425e447dd24a",
+    "url": "/covid_19_tracker.github.io/static/js/main.3e5867c1.chunk.js"
+  },
+  {
+    "revision": "a1699102713447d1fe80",
+    "url": "/covid_19_tracker.github.io/static/js/runtime-main.381c7979.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_19_tracker.github.io/static/media/image.d7265326.png"
+  }
+]);
